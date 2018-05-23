f2079e5ee7a591ce0b6bbfad7c4a463ad4dc6c83
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2018-05-23 22:46:57 +0100

    initial commit

---
+++ hello-play/app/controllers/EmployeeController.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import javax.inject._
+import models.Employee
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+/**
+  * Receive and post json
+  * @param cc
+  */
+@Singleton
+class EmployeeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+    * The method to expose sample json
+    * @param name
+    * @return json for employee
+    */
+  def employee(name: String) = Action.async {
+    implicit request: Request[AnyContent] =>
+      Future.successful(Ok(Json.toJson[Employee](Employee(name))))
+  }
+
+  /**
+    * The method to accept and parse json from request body
+    * NOTE we need to disable/set CSRF token
+    * @return employee attributes
+    */
+  def update = Action.async(parse.json) {
+    implicit request: Request[JsValue] =>
+      val employee = request.body.as[Employee]
+      Future.successful(Ok(employee.name + " " + employee.id))
+  }
+
+}
---
+++ hello-play/app/models/Employee.scala
@@ -0,0 +1,18 @@
+package models
+
+import play.api.libs.json.{JsValue, Json}
+
+case class Employee(name: String, id: Int)
+
+object Employee {
+
+  implicit val format = Json.format[Employee]
+
+  /*def writeEmployee(employee: Employee) = Json.toJson(employee)
+  def readEmployee(json: JsValue) = json.as[Employee]*/
+
+  def apply(name: String): Employee = {
+    val r = scala.util.Random
+    Employee(name, r.nextInt(100))
+  }
+}
