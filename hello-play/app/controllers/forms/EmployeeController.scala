771907ff447184bcda61c3e87e51213fcdf585a5
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2018-05-25 14:06:37 +0100

    added form submission

--- hello-play/app/controllers/forms/EmployeeController.scala
+++ hello-play/app/controllers/forms/EmployeeController.scala
@@ -2,15 +2,28 @@ package controllers.forms
 
 import controllers.forms.models.EmployeeForm
 import javax.inject.{Inject, Singleton}
+import play.api.i18n._
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
 import scala.concurrent.Future
 
 @Singleton
-class EmployeeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class EmployeeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
 
   def employeeDetails() = Action.async {
     implicit request: Request[AnyContent] =>
-      Future.successful(Ok(views.html.index()))
+      Future.successful(Ok(views.html.employeeForm(EmployeeForm.form)))
+  }
+
+  def employeeDetailsSubmission() = Action.async {
+    implicit request: Request[AnyContent] =>
+      EmployeeForm.form.bindFromRequest.fold(
+        formWithErrors => {
+          Future.successful(BadRequest(views.html.employeeForm(formWithErrors)))
+        },
+        employeeData => {
+          Future.successful(Ok("date we have received is " + employeeData.name + " " + employeeData.age))
+        }
+      )
   }
 }
