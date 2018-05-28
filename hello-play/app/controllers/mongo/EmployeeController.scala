ae1c6e2b021546869dbab303a75791a59fd5a892
gaur4vgaur <vaibgaurav@gmail.com>
2018-05-28 20:15:52 +0100

    remove record from Mongo

--- hello-play/app/controllers/mongo/EmployeeController.scala
+++ hello-play/app/controllers/mongo/EmployeeController.scala
@@ -1,16 +1,43 @@
 package controllers.mongo
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{ControllerComponents, AbstractController}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class EmployeeController @Inject() (cc: ControllerComponents, repository: EmployeeRepository) extends AbstractController(cc){
 
-  def createEmployee(name: String) = Action.async{
+  def createEmployee(name: String): Action[AnyContent] = Action.async{
     implicit request =>
       repository.addEmployee(Employee(name)).map { result =>
         Ok(result)
+      }
+  }
+
+  def mongoEmployeeWithAge(age: Int): Action[AnyContent] = Action.async {
+    implicit request =>
+      repository.employeeWithAge(age).map {
+        case Some(emp) => Ok(emp.toString)
+        case None => Ok("No employee with the age")
+      }
+  }
+
+  def mongoEmployees: Action[AnyContent] = Action.async {
+    implicit request =>
+      repository.employees.map(emps => Ok(emps.toString))
+  }
+
+  def mongoEmployeeCount: Action[AnyContent] = Action.async {
+    implicit request =>
+      repository.countEmployees.map(count => Ok(count.toString))
+  }
+
+  def removeEmployee(age: Int): Action[AnyContent] = Action.async {
+    implicit request =>
+      repository.remove(age).map {
+        case Some(emp) => Ok(emp.toString)
+        case None => Ok("No employee removed")
       }
   }
 
--- hello-play/app/controllers/mongo/EmployeeRepository.scala
+++ hello-play/app/controllers/mongo/EmployeeRepository.scala
@@ -1,9 +1,11 @@
 package controllers.mongo
 
 import javax.inject.Inject
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.{JsObject, Json, OFormat}
 import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.{Cursor, ReadPreference}
 import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.play.json._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
@@ -18,13 +20,33 @@ object Employee {
   }
 }
 
-class EmployeeRepository @Inject() (ec: ExecutionContext, reactiveMongoApi: ReactiveMongoApi) {
-  def collection(implicit ec: ExecutionContext): Future[JSONCollection] = reactiveMongoApi.database.map(_.collection("employee"))
+class EmployeeRepository @Inject()(reactiveMongoApi: ReactiveMongoApi) {
+  private def collection(implicit ec: ExecutionContext): Future[JSONCollection] = reactiveMongoApi.database.map(_.collection("employee"))
 
   def addEmployee(emp: Employee)(implicit ec: ExecutionContext): Future[String] = {
-    collection.flatMap(_.insert[Employee](emp)).map { x =>
+    collection.flatMap(_.insert[Employee](emp)).map { _ =>
       "Employee created successfully \n" + emp
     }
   }
+
+  def employeeWithAge(age: Int)(implicit ec: ExecutionContext): Future[Option[Employee]] =
+    collection.flatMap(_.find(Json.obj({
+      "age" -> age
+    })).one[Employee])
+
+  def employees(implicit ec: ExecutionContext): Future[Seq[Employee]] =
+    collection.flatMap(_.find(Json.obj())
+      .cursor[Employee]()
+      .collect[Seq](100, Cursor.FailOnError[Seq[Employee]]())
+    )
+
+  def countEmployees(implicit ec: ExecutionContext): Future[Int] =
+    collection.flatMap(_.count())
+
+  def remove(age: Int)(implicit ec: ExecutionContext): Future[Option[Employee]] =
+    collection.flatMap(_.findAndRemove(Json.obj({
+      "age" -> age
+    })).map(_.result[Employee]))
+
 
 }
